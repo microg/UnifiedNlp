65ccf61579c63b27a40ddf34812a4cb4e3783b35
mar-v-in <github@rvin.mooo.com>
2014-03-11 21:37:56 +0100

    Add build target for common build scenarios to Android.mk

--- Android.mk
+++ Android.mk
@@ -33,4 +33,53 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
+##
+# NetworkLocation
+# Target using com.google.android.gms as package name
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, api/src)
+
+LOCAL_JAVA_LIBRARIES := framework com.android.location.provider
+
+LOCAL_STATIC_JAVA_LIBRARIES := UnifiedNlpApi
+LOCAL_PACKAGE_NAME := NetworkLocation
+LOCAL_SDK_VERSION := current
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --rename-manifest-package com.google.android.gms
+
+include $(BUILD_PACKAGE)
+
+##
+# LegacyNetworkLocation
+# Target using com.google.android.location as package name
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, api/src)
+
+LOCAL_JAVA_LIBRARIES := framework com.android.location.provider
+
+LOCAL_STATIC_JAVA_LIBRARIES := UnifiedNlpApi
+LOCAL_PACKAGE_NAME := LegacyNetworkLocation
+LOCAL_SDK_VERSION := current
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --rename-manifest-package com.google.android.location
+
+include $(BUILD_PACKAGE)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
