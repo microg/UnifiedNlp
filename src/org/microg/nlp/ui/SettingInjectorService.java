80b6b280b6c71cb2d45b6166cd1eba7cf5e145c0
mar-v-in <github@rvin.mooo.com>
2015-02-11 12:21:39 +0100

    Add support for setting injection when in 4.4+ system image

---
+++ src/org/microg/nlp/ui/SettingInjectorService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013-2015 µg Project Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.microg.nlp.ui;
+
+import android.annotation.TargetApi;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+@TargetApi(Build.VERSION_CODES.KITKAT)
+public class SettingInjectorService extends android.location.SettingInjectorService {
+    private static final String TAG = "NlpSettingInjector";
+
+    public SettingInjectorService() {
+        super(TAG);
+    }
+
+    @Override
+    protected String onGetSummary() {
+        return "";
+    }
+
+    @Override
+    protected boolean onGetEnabled() {
+        PackageManager p = getPackageManager();
+        p.setComponentEnabledSetting(new ComponentName(this, SettingsActivity.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        return true;
+    }
+}
