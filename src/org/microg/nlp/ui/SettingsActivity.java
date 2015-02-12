09f0b33afa65b82d4700c106028c6c3d39cf7a06
mar-v-in <github@rvin.mooo.com>
2015-02-12 22:17:28 +0100

    Make it look native

--- src/org/microg/nlp/ui/SettingsActivity.java
+++ src/org/microg/nlp/ui/SettingsActivity.java
@@ -17,21 +17,16 @@
 package org.microg.nlp.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.preference.PreferenceFragment;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
 
 import org.microg.nlp.R;
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_wrapper, new MyPreferenceFragment()).commit();
     }
