ecdb26b6d5a0371177b4eee3dac64b43e7052ae9
mar-v-in <github@rvin.mooo.com>
2015-07-08 18:12:06 +0200

    Fix problems caused by theme

--- unifiednlp-base/src/main/java/org/microg/nlp/ui/SettingsActivity.java
+++ unifiednlp-base/src/main/java/org/microg/nlp/ui/SettingsActivity.java
@@ -19,14 +19,17 @@ package org.microg.nlp.ui;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.microg.nlp.R;
 
-public class SettingsActivity extends FragmentActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_wrapper, new MyPreferenceFragment()).commit();
     }
